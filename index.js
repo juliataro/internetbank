64e7f5e0ec0c63cee8d3dd0a4a37f7e2fc1ed3be
juliataro <julia.taro@gmail.com>
2021-10-14 17:18:58 +0300

    As a user I can transfer funds to another account [Delivers #179735565]

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express()
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
+const {processTransactions} = require("./middlewares");
 const swaggerDocument = yamljs.load('docs/swagger.yaml')
 
 
-require('dotenv').config()
+require('dotenv').config();
 
 // Serve API documentation on /docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -14,14 +15,22 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 // Parse request body as JSON into req.body
 app.use(express.json())
 
+//Endpoints
+app.use('/transactions/b2b', function (req, res){
+    console.log('Received incoming transfer request: ')
+    console.log('-Body: ' + JSON.stringify(req.body))
+    res.send({receiverName: 'Jaan Tamm'})
+})
 
-// /users endpoints
 app.use('/users', require('./routes/users'))
 app.use('/sessions', require('./routes/sessions'))
+app.use('/transactions', require('./routes/transactions'))
 
 mongoose.connect(process.env.MONGODB_URI, {}, function () {
     console.log('Connected to mongoDB')
 })
+
+processTransactions()
 
 app.listen(process.env.PORT, function () {
     console.log(`Listening on port ${process.env.PORT}`)
--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,15 @@
-
-
 const mongoose = require('mongoose')
+const Bank = require("./models/Bank")
+const Account = require("./models/Account")
 const Session = require("./models/Session")
+const Transaction = require("./models/Transaction")
+const fetch = require("node-fetch")
+const jose = require('node-jose')
+const fs = require('fs')
 
 exports.verifyToken = async (req, res, next) => {
 
-    // Check Authorization header existance
+    // Check Authorization header existence
     let authorizationHeader = req.header('Authorization')
     if (!authorizationHeader) {
         return res.status(401).send({error: 'Missing Authorization header'})
@@ -36,4 +40,186 @@ exports.verifyToken = async (req, res, next) => {
 
     return next(); // Pass the execution to the next middleware function
 
+}
+
+exports.refreshListOfBanksFromCentralBank = async function refreshListOfBanksFromCentralBank() {
+    console.log('Refreshing list of banks')
+
+    try {
+
+        //Attempt to get a list of banks in JSON format form central bank
+        let banks = await fetch(process.env.CENTRAL_BANK_URL, {
+            headers: {'Api-Key': process.env.CENTRAL_BANK_APIKEY}
+        }).then(responseText => responseText.json())
+
+        // delete all data from banks collection
+        await Bank.deleteMany()
+
+        // Initialize bulk operation object
+        const bulk = Bank.collection.initializeUnorderedBulkOp()
+
+        banks.forEach(bank => {
+            bulk.insert(bank)
+        })
+
+        // Bulk insert (in parallel) all prepared data to DB
+        await bulk.execute()
+
+        console.log('Done')
+        return true
+
+    } catch (e) {
+
+        //Handle error
+        console.log('Error:' + e.message)
+        return {error: e.message}
+    }
+
+}
+
+
+function isExpired(transaction) {
+    // check of the transaction has expired
+    const expireDate = new Date(
+        transaction.createdAt.getFullYear(),
+        transaction.createdAt.getMonth(),
+        transaction.createdAt.getDay() + 3
+    )
+
+
+    return new Date > expireDate;
+}
+
+async function setStatus(transaction, status, statusDetail) {
+
+    console.log('Setting transaction ' + transaction._id + ' as ' + status + (statusDetail ? ' (' + statusDetail + ')' : ''))
+    // Set transaction status to in progress
+    transaction.status = status
+    transaction.statusDetail = statusDetail
+    await transaction.save()
+}
+
+async function createSignedTransaction(input) {
+
+    let privateKey
+    try {
+        privateKey = fs.readFileSync('private.key', 'utf8')
+        const keystore = jose.JWK.createKeyStore();
+        const key = await keystore.add(privateKey, 'pem')
+        return await jose.JWS.createSign({format: 'compact'}, key).update(JSON.stringify(input), "utf8").final()
+    } catch (err) {
+        console.error('Error reading private key' + err)
+        throw Error('Error reading private key' + err)
+    }
+}
+
+async function sendRequestToBank(destinationBank, transactionAsJwt) {
+    return
+    response = await sendRequest(destinationBank.transactionUrl, {jwt: transactionAsJwt});
+}
+
+async function sendRequest(url, data) {
+    let responseText = '';
+
+    try {
+        let response = await fetch(url, {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        });
+
+
+        // Parse response body text
+        responseText = await response.text()
+
+        return JSON.parse(responseText);
+
+    } catch (e) {
+        throw Error(JSON.stringify({
+            exceptionMessage: e.message,
+            responseText
+        }))
+    }
+}
+
+async function refund(transaction) {
+    try {
+        const accountFrom = await Account.findOne({number: transaction.accountFrom})
+        console.log('Refunding transaction ' + transaction._id + ' by ' + transaction.amount)
+        accountFrom.balance += transaction.amount
+    } catch (e) {
+        console.log('Error refunding account: ')
+        console.log('Reason: ' + e.message)
+    }
+}
+
+exports.processTransactions = async function () {
+
+    // Get pending transactions
+    const pendingTransactions = await Transaction.find({status: 'Pending'})
+
+    // Loop through all pending
+    pendingTransactions.forEach(async transaction => {
+
+        console.log('Processing transaction ' + transaction._id)
+
+        // Assert that the transaction has not expired
+        if (isExpired(transaction)) {
+            await refund(transaction)
+            return await setStatus(transaction, 'Failed', 'Expired')
+        }
+
+        // Set transaction status to in progress ________________________________________________________________
+        await setStatus(transaction, 'In progress');
+
+        // Get the bank from accountTo
+        let bankPrefix = transaction.accountTo.substring(0, 3)
+        let destinationBank = await Bank.findOne({bankPrefix})
+
+        // If we don't have the bank in local database
+        if (!destinationBank) {
+            let result = exports.refreshListOfBanksFromCentralBank()
+            if (typeof result.error !== 'undefined') {
+                return await setStatus(transaction, 'Pending', 'Central bank refresh failed: ' + result.error)
+            }
+
+            destinationBank = Bank.setTraceFunction(traceFunction).findOne({bankPrefix});
+
+            if (!destinationBank) {
+                console.log(destinationBank.name)
+                await refund(transaction);
+                return await setStatus(transaction, 'Failed', 'Bank' + bankPrefix + ' does not exist')
+
+            }
+        }
+
+        try {
+            const response = await sendRequestToBank(destinationBank, await createSignedTransaction({
+                accountFrom: transaction.accountFrom,
+                accountTo: transaction.accountTo,
+                amount: transaction.amount,
+                currency: transaction.currency,
+                explanation: transaction.explanation,
+                senderName: transaction.senderName
+            }));
+
+            transaction.receiverName = response.receiverName
+            console.log('Completed transaction ' + transaction._id)
+            return await setStatus(transaction, 'Completed', '')
+
+
+        } catch (e) {
+
+            console.log('Error sending request to destination bank:')
+            console.log('- Transaction id is: ' + transaction._id)
+            console.log('- Error is: ' + e.message)
+            return await setStatus(transaction, 'Pending', e.message)
+
+        }
+
+    }, Error)
+
+
+    //Recursively call itself again
+    setTimeout(exports.processTransactions, 1000)
 }
